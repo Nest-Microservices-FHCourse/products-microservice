efd5256244adb16e9995433b9ce83c7c1f16d244
Luis Ramón Díaz <diazluisramon.dev@gmail.com>
2025-02-23 17:40:12 -0500

    add validation and DTOs for product creation

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,17 @@
-export class CreateProductDto {}
+import { IsNumber, IsString, Min } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CreateProductDto {
+  @IsString()
+  readonly name: string;
+
+  @IsString()
+  readonly description: string;
+
+  @IsNumber({
+    maxDecimalPlaces: 4,
+  })
+  @Min(0)
+  @Type(() => Number)
+  readonly price: number;
+}
